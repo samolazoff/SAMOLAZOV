f798b76d5774bbd910662cb842651c6f327010ea
Samolazoff <samolazov.roman.alex@gmail.com>
2025-11-17 20:10:38 +0300

    Add adaptive header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,21 +5,38 @@ import {navigation} from '../../data/data.json';
 import Logo from '../Logo/Logo';
 import NavItem from '../NavItem/NavItem';
 
+import Hamburger from 'hamburger-react';
+import {useState} from 'react';
+
 const Header = () => {
+
+    const [isOpen, setOpen] = useState(false);
+
+    const itemsNav = (navigation) => {
+        return(
+            <ul  className='app-header-nav-items'>
+                {
+                    navigation.map((itm, idx) => {
+                        const {title, link} = itm;
+                        return <NavItem title={title} link={link} key={idx}/>
+                    })
+                }
+            </ul>
+        )
+    }
+
     return (
         <header className='app-header'>
             <Logo/>
-            <nav className="app-header-nav">
-                <ul  className="app-header-nav-items">
-                    {
-                        navigation.map((itm, idx) => {
-                            const {title, link} = itm;
-                            return <NavItem title={title} link={link} key={idx}/>
-                        })
-                    }
-                </ul>
-                
+            <nav className='app-header-nav-ds'>
+                {itemsNav(navigation)}
             </nav>
+            <nav className={(!isOpen)?'app-header-nav  d-none':'app-header-nav'}>
+                {itemsNav(navigation)}
+            </nav>
+            <div className="wrap-hamburger">
+                <Hamburger toggled={isOpen} toggle={setOpen}/>
+            </div>
         </header>
     );
 };
