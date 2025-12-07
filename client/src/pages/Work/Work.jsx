9538fe901cc85d620c0411eee9e8dc654766fb28
Samolazoff <samolazov.roman.alex@gmail.com>
2025-12-07 21:25:26 +0300

    Create flexbility work blok

--- client/src/pages/Work/Work.jsx
+++ client/src/pages/Work/Work.jsx
@@ -4,7 +4,23 @@ import {Link} from 'react-router';
 
 import TitleBlock from '../../components/ui/TitleBlock/TitleBlock';
 
-import imgSite from '../../assets/images/work/site_1.png'
+import {works} from '../../data/data.json';
+
+const workItem = (link, title, description, img, idx) => {
+    return (
+        <li className='app-work-item' key={idx}>
+            <Link to={link}>
+                <div className="app-work-item-img-wrap">
+                    <img src={img} alt="img_site" className="app-work-item__img" />
+                </div>
+                <div className="app-work-item-discribe">
+                    <h3 className="app-work-item__title">{title}</h3>
+                    <span className="app-work-item__txt">{description}</span>
+                </div>
+            </Link>
+        </li>
+    )
+}
 
 const Work = () => {
     return (
@@ -12,39 +28,14 @@ const Work = () => {
             <TitleBlock txt='My work'/>
             <span className="app-work__subtitle">You can find my finished and current projects here. </span>
             <ul className="app-work-wrap">
-                <li className='app-work-item'>
-                    <Link to='/'>
-                        <div className="app-work-item-img-wrap">
-                            <img src={imgSite} alt="img_site" className="app-work-item__img" />
-                        </div>
-                        <div className="app-work-item-discribe">
-                            <h3 className="app-work-item__title">Samolazov</h3>
-                            <span className="app-work-item__txt">This is a multypage portfolio site for Samolazov. He is a web developer and freelancer</span>
-                        </div>
-                    </Link>
-                </li>
-                <li className='app-work-item'>
-                    <Link to='/'>
-                        <div className="app-work-item-img-wrap">
-                            <img src={imgSite} alt="img_site" className="app-work-item__img" />
-                        </div>
-                        <div className="app-work-item-discribe">
-                            <h3 className="app-work-item__title">Samolazov</h3>
-                            <span className="app-work-item__txt">This is a multypage portfolio site for Samolazov. He is a web developer and freelancer</span>
-                        </div>
-                    </Link>
-                </li>
-                <li className='app-work-item'>
-                    <Link to='/'>
-                        <div className="app-work-item-img-wrap">
-                            <img src={imgSite} alt="img_site" className="app-work-item__img" />
-                        </div>
-                        <div className="app-work-item-discribe">
-                            <h3 className="app-work-item__title">Samolazov</h3>
-                            <span className="app-work-item__txt">This is a multypage portfolio site for Samolazov. He is a web developer and freelancer</span>
-                        </div>
-                    </Link>
-                </li>
+                {
+                    works.map((itm, idx) => {
+                        const {link, title, description, img} = itm;
+                        return(
+                            workItem(link, title, description, img, idx)
+                        )
+                    })
+                }
             </ul>
         </section>
     );
