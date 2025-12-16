ad6026898dfe857d826ed6edbcd63febf1142a7b
Samolazoff <samolazov.roman.alex@gmail.com>
2025-12-16 12:28:47 +0300

    Start to layout block contact

---
+++ client/src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+import './Contacts.scss';
+
+import TitleBlock from '../../components/ui/TitleBlock/TitleBlock';
+
+const Contacts = () => {
+    return (
+        <section className='app-contacts'>
+            <TitleBlock txt='Contacts'/>
+        </section>
+    );
+};
+
+export default Contacts;
--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -5,9 +5,7 @@ import Layout from "../components/Layout/Layout";
 import About from "../pages/About/About";
 import Work from "../pages/Work/Work";
 import BLog from "../pages/Blog/Blog";
-
-
-// import Contacts from '../pages/Contacts/Contacts';
+import Contacts from '../pages/Contacts/Contacts';
 
 const AppRoutes = () => {
     return (
@@ -17,7 +15,7 @@ const AppRoutes = () => {
                     <Route index element = {<About/>}/>
                     <Route path="work" element={<Work/>}/>
                     <Route path="blog" element={<BLog/>}/>
-                    {/* <Route path="contacts" element = {<Contacts/>}/> */}
+                    <Route path="contacts" element = {<Contacts/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
