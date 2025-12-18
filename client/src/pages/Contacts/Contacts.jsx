f3ad515540cd84d486aa2a1a9294a3188639dbec
Samolazoff <samolazov.roman.alex@gmail.com>
2025-12-18 12:49:33 +0300

    Add contacts-main in contacts

--- client/src/pages/Contacts/Contacts.jsx
+++ client/src/pages/Contacts/Contacts.jsx
@@ -3,6 +3,10 @@ import './Contacts.scss';
 import TitleBlock from '../../components/ui/TitleBlock/TitleBlock';
 import ContactsSocial from '../../components/ContactsSocial/ContactsSocial';
 
+import imgEmail from '../../assets/icons/icons8-email.png';
+import imgTelegram from '../../assets/icons/icons8-telegram.svg';
+import imgWhatsapp from '../../assets/icons/icons8-whatsapp.svg';
+
 const Contacts = () => {
     return (
         <section className='app-contacts'>
@@ -11,15 +15,15 @@ const Contacts = () => {
                 <div className="app-contacts-main">
                     <ul className="app-contacts-list">
                         <li className="app-contacts-item">
-                            <img src=" " alt="telegram-icon" className="app-contacts-item__img" />
+                            <img src={imgTelegram} alt="telegram-icon" className="app-contacts-item__img" />
                             <span className="app-contacts-item__txt">@SamolazovRoman</span>
                         </li>
                         <li className="app-contacts-item">
-                            <img src=" " alt="whatsapp-icon" className="app-contacts-item__img" />
+                            <img src={imgWhatsapp} alt="whatsapp-icon" className="app-contacts-item__img" />
                             <span className="app-contacts-item__txt">+48 789 047 614</span>
                         </li>
                         <li className="app-contacts-item">
-                            <img src=" " alt="email-icon" className="app-contacts-item__img" />
+                            <img src={imgEmail} alt="email-icon" className="app-contacts-item__img" />
                             <span className="app-contacts-item__txt">samalazau.raman@proton.me</span>
                         </li>
                     </ul>
