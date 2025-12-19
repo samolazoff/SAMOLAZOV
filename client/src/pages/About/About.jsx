a0e3245c82f4d76a49ec93f53b1ce7dc5dc985bd
Samolazoff <samolazov.roman.alex@gmail.com>
2025-12-19 12:02:19 +0300

    Add mini-blog in block about

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,12 +1,30 @@
 import './About.scss';
 
+import {Link} from 'react-router';
+
 import TitleBlock from '../../components/ui/TitleBlock/TitleBlock';
 import SubtitleBlock from '../../components/ui/SubtitleBlock/SubtitleBlock';
 import Btn from '../../components/ui/Btn/Btn';
 
 import imgMine from '../../assets/images/about/5373046436129799659.png';
 
+import {works, blog} from '../../data/data.json';
+
+const miniBlogPost = (item) => {
+
+    const {id, title, data} = item;
+
+    return (
+        <li key={id} id={id} className='blog-mini-item'>
+            <data value={data}>{data}</data>
+            <Link to={id} className='blog-mini-item__title'>{title}</Link>
+        </li>
+    )
+}
+
+
 const About = () => {
+
     return (
         <>
             <section className='app-about'>
@@ -17,10 +35,27 @@ const About = () => {
                     <span className="app-about-content__txt">I enjoy creating delightful, human-centered digital experiences. You have only one chance to become a full-fledged part of the Internet.</span>
                     <span className="app-about-content__txt app-about-content__txt_right">Let’s make it an amazing one!</span>
                     <Btn tage='link' href='#contacts-form' txt='Contacts Me'/>
-            </div>
+                </div>
             </section>
             <section className="mini-pages">
-                
+                <div className="work-mini">
+                    <h3 className="title-block-mini">Last work</h3>
+                    
+                    <Btn tage='link' href='/work' txt='More Works'/>
+                </div>
+                <div className="blog-mini">
+                    <h3 className="title-block-mini">News</h3>.
+                    <ul className="blog-mini-list">
+                        {
+                            blog.map((itm) => {
+                                return(
+                                   miniBlogPost(itm)
+                                )
+                            })
+                        }
+                    </ul>
+                    <Btn tage='link' href='/blog' txt='More News'/>
+                </div>
             </section>
         </>
         
